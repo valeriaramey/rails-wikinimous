b19b1ab5913973e1bbe5e23e0f6ccc7169c4e0ac
Valeria Ramey <valeriaramey@gmail.com>
2020-08-11 18:01:08 -0400

    crud complete

--- Gemfile
+++ Gemfile
@@ -47,5 +47,8 @@ group :test do
   gem 'webdrivers'
 end
 
+gem 'faker'
+gem 'simple_form'
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+   # index in the browser to list all articles
+  get 'articles', to: 'articles#index'
+  # display NEW article form in the browser
+  get 'articles/new', to: 'articles#new'
+  # Create a new articl in the serrver
+  post 'articles', to: 'articles#create'
+  # Edit FORM to update the article
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit
+  # UPDATE in the server
+  patch 'articles/:id', to: 'articles#update'
+  # to read one article
+  get 'articles/:id', to: 'articles#show', as: :article
+  # delete an article
+  delete 'articles/:id', to: 'articles#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
